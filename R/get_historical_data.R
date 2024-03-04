da9b639c21e9d0dc4fc73b744c2e7eb18ec3e9a2
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2024-03-04 15:19:16 +0000

    first version of template

--- R/get_historical_data.R
+++ R/get_historical_data.R
@@ -14,7 +14,7 @@ hv_read_historical <- function(AR, SYNAFLOKKUR) {
     dplyr::left_join(mardata::syni,
                      by = "stod_id") |>
     dplyr::filter(synaflokkur_nr %in% SYNAFLOKKUR) |>
-    dplyr::mutate(index = reitur * 100 + tog_nr) |>
+    dplyr::mutate(index = (reitur * 100 + tog_nr) * 100 + veidarfaeri) |>
     dplyr::select(synis_id,
                   leidangur,
                   dags,
@@ -72,26 +72,46 @@ hv_read_historical <- function(AR, SYNAFLOKKUR) {
                   toglengd = ifelse(is.na(toglengd), 4, toglengd))
   
   NU <-
-    mardata::skala |>
-    dplyr::filter(synis_id %in% ST$synis_id) |>
-    dplyr::select(synis_id,
+    ST |> 
+    dplyr::select(synis_id, ar, index) |> 
+    dplyr::left_join(mardata::skala |>
+                        # strange to have na in tegund_nr
+                        dplyr::filter(!is.na(tegund_nr)),
+                     by = dplyr::join_by(synis_id)) |>
+    dplyr::select(ar, 
+                  index,
                   tegund = tegund_nr,
                   fj_maelt = maeldir,
                   fj_talid = taldir) |>
-    dplyr::mutate(fj_alls = fj_maelt + fj_talid)
-  
+    dplyr::mutate(fj_alls = fj_maelt + fj_talid,
+                  r = dplyr::case_when(fj_maelt == 0 ~ 1,
+                                       .default = fj_alls / fj_maelt))
   LE <-
-    mardata::lengd |>
-    dplyr::filter(synis_id %in%ST$synis_id) |>
-    dplyr::select(synis_id,
-                  tegund = tegund_nr,
-                  lengd,
-                  fjoldi)
-  
+    ST |> 
+    dplyr::select(synis_id, ar, index) |> 
+    dplyr::left_join(mardata::lengd,
+                      by = dplyr::join_by(synis_id)) |> 
+    dplyr::group_by(ar, index, tegund = tegund_nr, lengd) |> 
+    dplyr::summarise(fjoldi = sum(fjoldi, na.rm = TRUE),
+                     .groups = "drop") |> 
+    dplyr::left_join(NU |> 
+                       dplyr::select(ar, index, tegund, r),
+                     by = dplyr::join_by(ar, index, tegund)) |> 
+    dplyr::mutate(n = fjoldi * r,
+                  b = (n * 0.01 * lengd^3) / 1e3) |> 
+    # some odd measure
+    dplyr::filter(!(ar == 1993 & index == 6660273 & tegund == 41 & is.na(r))) |> 
+    dplyr::filter(!(ar == 1986 & index == 5630273 & tegund == 19 & is.na(lengd))) 
+  if(any(is.na(LE$r))) stop("Unexpected: Raising factor (r) in object LE is na")
+  if(any(is.na(LE$lengd))) stop("Unexpected: Undefined lengd in object LE")
+         
   KV <-
-    mardata::aldur |>
-    dplyr::filter(synis_id %in%ST$synis_id) |>
-    dplyr::select(synis_id,
+    ST |> 
+    dplyr::select(synis_id, ar, index) |> 
+    dplyr::inner_join(mardata::aldur,
+                      by = dplyr::join_by(synis_id)) |> 
+    dplyr::select(ar,
+                  index,
                   tegund = tegund_nr,
                   nr = kvarna_nr,
                   lengd,
--- R/get_zip_data.R
+++ R/get_zip_data.R
@@ -9,10 +9,10 @@
 #' @export
 #'
 hv_read_zip_data <- function(zip_file) {
- 
+  
   res <- purrr::map(zip_file, hv_read_zip_file)
   names(res) <- basename(zip_file)
-
+  
   return(list(ST = dplyr::bind_rows(sapply(res, "[", "ST")), 
               M  = dplyr::bind_rows(sapply(res, "[", "M")), 
               NU = dplyr::bind_rows(sapply(res, "[", "NU")), 
@@ -38,19 +38,23 @@ hv_read_zip_file <- function(zip_file) {
   }
   
   tmpdir <- tempdir()
-  if(file.exists(paste0(tmpdir, "/hafvog.leidangrar.txt"))) file.remove(paste0(tmpdir, "/hafvog.leidangrar.txt"))
-  if(file.exists(paste0(tmpdir, "/hafvog.skraning.txt"))) file.remove(paste0(tmpdir, "/hafvog.skraning.txt"))
-  if(file.exists(paste0(tmpdir, "/hafvog.stodvar.txt"))) file.remove(paste0(tmpdir, "/hafvog.stodvar.txt"))
-  if(file.exists(paste0(tmpdir, "/hafvog.togstodvar.txt"))) file.remove(paste0(tmpdir, "/hafvog.togstodvar.txt"))
-  if(file.exists(paste0(tmpdir, "/hafvog.umhverfi.txt"))) file.remove(paste0(tmpdir, "/hafvog.umhverfi.txt"))
-  if(file.exists(paste0(tmpdir, "/hafvog.skraning.txt"))) file.remove(paste0(tmpdir, "/hafvog.skraning.txt"))
+  #if(file.exists(paste0(tmpdir, "/hafvog.leidangrar.txt"))) file.remove(paste0(tmpdir, "/hafvog.leidangrar.txt"))
+  #if(file.exists(paste0(tmpdir, "/hafvog.skraning.txt"))) file.remove(paste0(tmpdir, "/hafvog.skraning.txt"))
+  #if(file.exists(paste0(tmpdir, "/hafvog.stodvar.txt"))) file.remove(paste0(tmpdir, "/hafvog.stodvar.txt"))
+  #if(file.exists(paste0(tmpdir, "/hafvog.togstodvar.txt"))) file.remove(paste0(tmpdir, "/hafvog.togstodvar.txt"))
+  #if(file.exists(paste0(tmpdir, "/hafvog.umhverfi.txt"))) file.remove(paste0(tmpdir, "/hafvog.umhverfi.txt"))
+  #if(file.exists(paste0(tmpdir, "/hafvog.skraning.txt"))) file.remove(paste0(tmpdir, "/hafvog.skraning.txt"))
   utils::unzip(zip_file, exdir = tmpdir)
   
   ## station -------------------------------------------------------------------
   ST <- 
     hafvog::hv_station(tmpdir, std = FALSE) |> 
-    #dplyr::filter(synaflokkur %in% id) |>
-    dplyr::mutate(index = reitur * 100 + tognumer) |> 
+    dplyr::rename(veidarfaeri = fishing_gear_no) |> 
+    # sometimes people forget to put in gear - should really generate a warning
+    dplyr::mutate(vf = dplyr::case_when(synaflokkur == 30 & is.na(veidarfaeri) ~ 73,
+                                        .default = veidarfaeri),
+                  index = (reitur * 100 + tognumer) * 100 + vf) |>
+    dplyr::select(-vf) |> 
     dplyr::mutate(lon1 = kastad_v_lengd,
                   lat1 = kastad_n_breidd,
                   lon2 = hift_v_lengd,
@@ -60,9 +64,14 @@ hv_read_zip_file <- function(zip_file) {
                   toglengd = ifelse(is.na(toglengd), 4, toglengd),
                   veidarfaeri = 73) |> 
     dplyr::mutate(ar = lubridate::year(dags))
+  if(any(is.na(ST$index))) stop("Unexpected: Tow index (index) is na")
   ## Measures ------------------------------------------------------------------
   M <- 
-    hv_measures(tmpdir, std = FALSE) |> 
+    ST |> 
+    dplyr::select(synis_id, ar, index) |> 
+    dplyr::left_join(hv_measures(tmpdir, std = FALSE),
+                     by = dplyr::join_by(synis_id)) |>
+    dplyr::select(-synis_id) |> 
     # maeliadgerd 30 is tagging - would not normally expect that in SMX
     #   expect tagging to be in another synaflokkur than survey
     dplyr::mutate(m = dplyr::case_when(maeliadgerd %in% c(1:3, 30) ~ "maelt",
@@ -71,35 +80,48 @@ hv_read_zip_file <- function(zip_file) {
   ## Numer -----------------------------------------------------------------------
   NU <- 
     ST |> 
-    dplyr::select(synis_id, index) |> 
-    dplyr::left_join(M |> dplyr::filter(m %in% c("maelt", "talid")),
-                     by = dplyr::join_by(synis_id)) |> 
-    dplyr::group_by(synis_id, index, tegund, m) |> 
+    dplyr::select(ar, index) |> 
+    dplyr::left_join(M |> 
+                       dplyr::filter(m %in% c("maelt", "talid")),
+                     by = dplyr::join_by(ar, index)) |> 
+    dplyr::group_by(ar, index, tegund, m) |> 
     dplyr::summarise(n = sum(fjoldi),
                      .groups = "drop") |> 
     tidyr::pivot_wider(names_from = m, values_from = n, values_fill = 0) |> 
     dplyr::mutate(alls = maelt + talid) |> 
-    dplyr::arrange(index, tegund) |> 
     dplyr::rename(fj_maelt = maelt,
                   fj_talid = talid,
-                  fj_alls = alls)
+                  fj_alls = alls) |> 
+    dplyr::mutate(r = dplyr::case_when(fj_maelt == 0 ~ 1,
+                                       .default = fj_alls / fj_maelt))
+  
+  
   ## Length ----------------------------------------------------------------------
   LE <-
     ST |> 
-    dplyr::select(synis_id, index) |> 
+    dplyr::select(ar, index) |> 
     dplyr::left_join(M |> 
                        dplyr::filter(m %in% "maelt") |> 
-                       dplyr::group_by(synis_id, tegund, lengd) |> 
+                       dplyr::group_by(ar, index, tegund, lengd) |> 
                        dplyr::summarise(fjoldi = sum(fjoldi, na.rm = TRUE),
                                         .groups = "drop"),
-                     by = dplyr::join_by(synis_id))
+                     by = dplyr::join_by(ar, index)) |> 
+    # skala með toldum
+    dplyr::left_join(NU |> 
+                       dplyr::select(ar, index, tegund, r),
+                     by = dplyr::join_by(ar, index, tegund)) |> 
+    dplyr::mutate(n = r * fjoldi,
+                  b = (n * 0.01 * lengd^3) / 1e3)
+  if(any(is.na(LE$r))) stop("Unexpected: Raising factor (r) is na")
+  
   ## Kvarnir ---------------------------------------------------------------------
   KV <- 
     ST |> 
-    dplyr::select(synis_id, index) |> 
-    dplyr::left_join(M |> dplyr::filter(maeliadgerd == 3),
-                     by = dplyr::join_by(synis_id)) |> 
-    dplyr::select(synis_id, index, tegund, nr, lengd, kyn,
+    dplyr::select(ar, index) |> 
+    dplyr::left_join(M |> 
+                       dplyr::filter(maeliadgerd == 3),
+                     by = dplyr::join_by(ar, index)) |> 
+    dplyr::select(ar, index, tegund, nr, lengd, kyn,
                   kynthroski, oslaegt, slaegt,
                   kynfaeri, lifur)
   
@@ -107,14 +129,14 @@ hv_read_zip_file <- function(zip_file) {
   pred <- 
     M |> 
     dplyr::filter(!is.na(magaastand)) |> 
-    dplyr::select(synis_id, pred = tegund, nr, oslaegt, slaegt, 
+    dplyr::select(ar, index, pred = tegund, nr, oslaegt, slaegt, 
                   astand = magaastand) 
-    # # Need the support table upstream
-    # dplyr::left_join(magaastand |> 
-    #                    dplyr::select(astand, lysing_astands),
-    #                  by = "astand") |> 
-    # dplyr::select(-astand) |> 
-    # dplyr::rename(astand = lysing_astands)
+  # # Need the support table upstream
+  # dplyr::left_join(magaastand |> 
+  #                    dplyr::select(astand, lysing_astands),
+  #                  by = "astand") |> 
+  # dplyr::select(-astand) |> 
+  # dplyr::rename(astand = lysing_astands)
   ## Prey (not really needed) --------------------------------------------------
   prey <-
     M |> 
@@ -123,8 +145,11 @@ hv_read_zip_file <- function(zip_file) {
                   pred = ranfiskurteg,
                   pnr = nr, 
                   nr = kvarnanr) |>
-    dplyr::select(synis_id, pred, nr, prey, pnr, 
+    dplyr::select(ar, index, pred, nr, prey, pnr, 
                   n = fjoldi, lengd, kyn, thyngd = heildarthyngd)
+  
+  # downstream just use ar index as a link
+  ST <- ST |> dplyr::select(-synis_id)
   
   return(list(ST = ST, M = M, NU = NU, LE = LE, KV = KV, pred = pred, prey = prey))
 }
